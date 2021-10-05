63f51961671542e25c8c6024d229f544549a6826
JayclyhLi_corssair <jayclyhli@gmail.com>
2021-10-05 11:14:29 -0700

    test

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template  # Import Flask to allow us to create our app
+
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return render_template('index.html', name=)  
+
+@app.route('/success')
+def success():
+    print('hi')
+    return "success"
+
+@app.route('/hello/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+def hello(name):
+    print(name)
+    return "Hello, " + name
+
+@app.route('/users/<username>/<int:id>') # for a route '/users/____/____', two parameters in the url get passed as username and id
+def show_user_profile(username, id):
+    print(username)
+    print(id)
+    return "username: " + username + ", id: " + id
+
+####
+####
+
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
